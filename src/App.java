1e1976983668e8fc636d22531383d4634f10ae47
Lucas Martins <l.martins90@edu.pucrs.br>
2020-09-30 16:40:52 -0300

    Criação da classe ArrayListOfAcidente

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import Entities.Acidente;
+import Lists.ArrayListOfAcidente;
 import Utils.CsvToObject;
 
 import java.util.List;
@@ -7,7 +8,23 @@ public class App {
     public static void main(String[] args){
 
         String[] lines = CsvToObject.readCsv("E:\\PUCRS 2020.2\\46502-04 ALEST I\\RepoAlest_1\\Trabalho\\Arquivos\\acidentes.csv");
-        List<Acidente> acidentes = CsvToObject.retornaAcidentes(lines);
-        System.out.println(acidentes.size());
+        ArrayListOfAcidente acidentes = new ArrayListOfAcidente(100000);
+        ArrayListOfAcidente acidentes2 = new ArrayListOfAcidente(100000);
+
+        for(String line : lines){
+            if(CsvToObject.retornaAcidente(line) != null){
+//                if(!acidentes.addIncreasingOrder(CsvToObject.retornaAcidente(line))){
+//                    System.out.println(CsvToObject.retornaAcidente(line));
+//                }
+                acidentes.add(CsvToObject.retornaAcidente(line));
+            }
+            acidentes2.add(CsvToObject.retornaAcidente(line));
+        }
+
+        System.out.println("Tamanho Lista acidentes1: "+ acidentes.size());
+        System.out.println("Tamanho Lista acidentes2: "+ acidentes2.size());
+
+        System.out.println("Lista acidentes1 pos 91381: "+ acidentes.get(91381));
+        System.out.println("Lista acidentes2 pos 99999: "+ acidentes2.get(99999));
     }
 }
